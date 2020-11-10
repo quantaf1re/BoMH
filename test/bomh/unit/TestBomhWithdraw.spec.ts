7a2911d72a1845f7943f5c13c15d9005b06fcfb5
Quantaf1re <37821211+quantaf1re@users.noreply.github.com>
2020-11-10 15:29:32 +0100

    Streamlining

--- test/bomh/unit/TestBomhWithdraw.spec.ts
+++ test/bomh/unit/TestBomhWithdraw.spec.ts
@@ -17,33 +17,26 @@ contract("Bank of Moral Hazard Withdrawals", async (accounts) => {
             console.log("Initiating BoMH");
             await bm.init(accounts);
         }
-    });
-
-    beforeEach("creating snapshot", async () => {
-        snapshot = await createSnapshot();
         await bm.bomh.deposit(bm.tusd.address, bc.DEP_AMOUNT, { from: bm.sa.dummy1});
     });
 
-    afterEach("reverting to snapshot", async () => {
-        await revertToSnapshot(snapshot);
-    });
+    describe("--------BUG 2 CASE 1 SHOWCASE - CHECK CODE--------", async () => {
 
-    
-    describe("--------BUG SHOWCASE - CHECK CODE--------", async () => {
-        it("Case 1", async () => {
+        let withdrawTx: any;
+
+        before(async () => {
             console.log(`
             -----------------NOTE-----------------
             There are 2 bugs I've discovered with 2 cases.
             Case 1: withdrawing straight after depositing
             Case 2: withdrawing after there have been some swaps
             by a 3rd party after depositing
-
-            This branch focuses on bug 2
-
+    
             To compensate for bug 1, perhaps we should subtract 1 from the
             value returned by getSaveRedeemInput(). This branch has done
-            that in bomh.sol
-
+            that in bomh.sol. This allows withdraw() to execute without
+            error, but means that users receive 1wei less than they should.
+    
             ---Without any swapping before---
             getSaveRedeemInput() = 
             ${await bm.mstHelp.getSaveRedeemInput(bm.mUSDSAVE.address, bc.LENT_AMOUNT)}
@@ -53,10 +46,39 @@ contract("Bank of Moral Hazard Withdrawals", async (accounts) => {
             with 'Saver has no credits'.
             The credit balance of bomh:
             ${await bm.mUSDSAVE.creditBalances(bm.bomh.address)}
-
+    
             `);
+    
+            withdrawTx = await bm.bomh.withdraw(bc.DEP_AMOUNT, { from: bm.sa.dummy1 });
+        });
+    
+        it("Valid withdraw1", async () => {
+            expect(await bm.tusd.balanceOf(bm.bomh.address)).bignumber.eq(ZERO);
+        });
 
-            await bm.bomh.withdraw(bc.DEP_AMOUNT, { from: bm.sa.dummy1 });
+        it("Valid withdraw2", async () => {
+            const feePaid = bc.LENT_AMOUNT.mul(bm.swapFee).div(fullScale);
+            expect(await bm.tusd.balanceOf(bm.sa.dummy1)).bignumber.eq(bc.SC_INIT_AMOUNT.sub(feePaid));
+        });
+
+        it("Valid withdraw3", async () => {
+            expect(await bm.mstHelp.getSaveBalance(bm.mUSDSAVE.address, bm.bomh.address))
+                .bignumber.eq(ZERO);
+        });
+
+        it("Valid withdraw4", async () => {
+            expect(await bm.mhUSD.balanceOf(bm.sa.dummy1)).bignumber.eq(ZERO);
+        });
+
+        it("Valid withdraw5", async () => {
+            expect(await bm.mhUSD.totalSupply()).bignumber.eq(ZERO);
+        });
+
+        it("Valid withdraw6", async () => {
+            await expectEvent(withdrawTx.receipt, "Withdrawn", {
+                withdrawer: bm.sa.dummy1,
+                bassetQuantity: bc.DEP_AMOUNT,
+            });
         });
     });
 });
